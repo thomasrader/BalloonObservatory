9cce2146a2bb141f8d35fcd2b9b84b10b7a71bcd
popcornhobo <TooCoolDevelopment@Tys-MacBook-Pro.local>
2015-10-31 15:23:45 -0600

    Adding some actual code

---
+++ ServoControl_CSD.c
@@ -0,0 +1,59 @@
+//	USER INCLUDES	//
+#include "ServoControl_CSD.h"
+#include "DynamixelMX12.h"
+//-----------------------------------------------------------------------//
+
+int main() {
+	
+	printf("Starting\n");
+	//----------------------------------------------------------------------//
+	//	INITIALIZE PORT REGISTERS	//
+	
+	void *virtual_base;
+	int fd;	
+	
+	if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) {
+		printf( "ERROR: could not open \"/dev/mem\"...\n" );
+		return( 1 );
+	}
+
+	virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, HW_REGS_BASE );
+
+	if( virtual_base == MAP_FAILED ) {
+		printf( "ERROR: mmap() failed...\n" );
+		close( fd );
+		return( 1 );
+	}
+	//----------------------------------------------------------------------//
+	
+	//	ADDITIONAL MEMORY MAPPING	//
+	
+	UART_Map(virtual_base);
+	//----------------------------------------------------------------------//
+	
+	// LOCAL VARIABLES	//
+	uint8_t Data_out[15];
+	//----------------------------------------------------------------------//
+	//	FUNCTIONAL CODE	//
+	
+	while(1)
+	{
+		Set_Position(SERVO1,200,Data_out);
+		usleep(10000);
+		Get_Position(SERVO1,Data_out);
+		printf("DataOut: %s\n",Data_out);
+	}
+	
+	//----------------------------------------------------------------------//
+	//	CLEAN UP MEMORY MAPPING	//
+	
+	if( munmap( virtual_base, HW_REGS_SPAN ) != 0 ) {
+		printf( "ERROR: munmap() failed...\n" );
+		close( fd );
+		return( 1 );
+	}
+
+	close( fd );
+	//----------------------------------------------------------------------//
+	return( 0 );
+}
