aabd0971a58797a6e8e8090becfcbe15ccf365c7
zacharysfrazee <zach@trailsendrepair.com>
2015-11-18 19:45:09 -0700

    Added Complementary Filter

--- C_Project/BNO055.c
+++ C_Project/BNO055.c
@@ -29,6 +29,77 @@ void Get_Orientation(int IMU_data[])
 	IMU_data[2] = (I2C_ReadByte(0x28, EULER_YAW_H, PIO_Data, PIO_DDIR, &failed) << 8) | (I2C_ReadByte(0x28, EULER_YAW_L, PIO_Data, PIO_DDIR, &failed));
 }
 
+void Get_Complementarty_Orientation(int IMU_data[], float Delta_time){
+	static float pitch = 0;
+	static float roll = 0;
+	static float yaw = 0;
+	
+	//Failure flag
+	uint8_t failed = 0;
+	
+	uint16_t Accel_x_int	= (I2C_ReadByte(0x28, ACCEL_X_H, PIO_Data, PIO_DDIR, &failed) << 8) | (I2C_ReadByte(0x28, ACCEL_X_L, PIO_Data, PIO_DDIR, &failed));
+	uint16_t Accel_y_int	= (I2C_ReadByte(0x28, ACCEL_Y_H, PIO_Data, PIO_DDIR, &failed) << 8) | (I2C_ReadByte(0x28, ACCEL_Y_L, PIO_Data, PIO_DDIR, &failed));
+	uint16_t Accel_z_int	= (I2C_ReadByte(0x28, ACCEL_Z_H, PIO_Data, PIO_DDIR, &failed) << 8) | (I2C_ReadByte(0x28, ACCEL_Z_L, PIO_Data, PIO_DDIR, &failed));
+	
+	//printf("Accel_x_u: %d\n", Accel_x_int);
+	//printf("Accel_y_u: %d\n", Accel_y_int);
+	//printf("Accel_z_u: %d\n", Accel_z_int);
+	
+	uint16_t Mag_x_int 	= (I2C_ReadByte(0x28, MAG_X_H, PIO_Data, PIO_DDIR, &failed) << 8) | (I2C_ReadByte(0x28, MAG_X_L, PIO_Data, PIO_DDIR, &failed));
+	uint16_t Mag_y_int 	= (I2C_ReadByte(0x28, MAG_Y_H, PIO_Data, PIO_DDIR, &failed) << 8) | (I2C_ReadByte(0x28, MAG_Y_L, PIO_Data, PIO_DDIR, &failed));
+	uint16_t Mag_z_int 	= (I2C_ReadByte(0x28, MAG_Z_H, PIO_Data, PIO_DDIR, &failed) << 8) | (I2C_ReadByte(0x28, MAG_Z_L, PIO_Data, PIO_DDIR, &failed));
+	
+	//printf("Mag_x_u: %d\n", Mag_x_int);
+	//printf("Mag_y_u: %d\n", Mag_y_int);
+	//printf("Mag_z_u: %d\n", Mag_z_int);
+	
+	uint16_t Gyro_x_int	= (I2C_ReadByte(0x28, GYRO_X_H, PIO_Data, PIO_DDIR, &failed) << 8) | (I2C_ReadByte(0x28, GYRO_X_L, PIO_Data, PIO_DDIR, &failed)); 
+	uint16_t Gyro_y_int	= (I2C_ReadByte(0x28, GYRO_Y_H, PIO_Data, PIO_DDIR, &failed) << 8) | (I2C_ReadByte(0x28, GYRO_Y_L, PIO_Data, PIO_DDIR, &failed)); 
+	uint16_t Gyro_z_int	= (I2C_ReadByte(0x28, GYRO_Z_H, PIO_Data, PIO_DDIR, &failed) << 8) | (I2C_ReadByte(0x28, GYRO_Z_L, PIO_Data, PIO_DDIR, &failed));
+	
+	//printf("Gyro_x_u: %d\n", Gyro_x_int);
+	//printf("Gyro_y_u: %d\n", Gyro_y_int);
+	//printf("Gyro_z_u: %d\n", Gyro_z_int);
+	
+	int Accel_x = ((int16_t)Accel_x_int);
+	int Accel_y = ((int16_t)Accel_y_int);
+	int Accel_z = ((int16_t)Accel_z_int);
+	
+	//printf("Accel_x: %d\n", Accel_x);
+	//printf("Accel_y: %d\n", Accel_y);
+	//printf("Accel_z: %d\n", Accel_z);
+	
+	int Mag_x 	= ((int16_t)Mag_x_int);
+	int Mag_y 	= ((int16_t)Mag_y_int);
+	int Mag_z 	= ((int16_t)Mag_z_int);
+	
+	float Gyro_x = ((float)((int16_t)Gyro_x_int)/(float)16) * (PI/(float)180);
+	float Gyro_y = ((float)((int16_t)Gyro_y_int)/(float)16) * (PI/(float)180);
+	float Gyro_z = ((float)((int16_t)Gyro_z_int)/(float)16) * (PI/(float)180);  
+
+	//printf("Gyro_x: %lf\n", Gyro_x);
+	//printf("Gyro_y: %lf\n", Gyro_y);
+	//printf("Gyro_z: %lf\n", Gyro_z);
+	
+	roll =  GYRO_WEIGHT * (roll + Gyro_x * Delta_time) + (1 - GYRO_WEIGHT)*atan2((double)Accel_y,(double)Accel_z);
+	
+	//printf("Roll: %lf\t", roll);
+	
+	pitch = GYRO_WEIGHT * (pitch + Gyro_y * Delta_time) + (1 - GYRO_WEIGHT)*atan2(-1*Accel_x, (Accel_y*sin(roll) + Accel_z*cos(roll))); 
+	
+	//printf("Pitch: %lf\t", pitch);
+	
+	float Mag_heading = atan2(Mag_z * sin(roll) - Mag_y*cos(roll), (Mag_x*cos(pitch) + Mag_y*sin(pitch)*sin(roll) + Mag_z*sin(pitch)*cos(roll)));
+	
+	yaw = GYRO_WEIGHT * (yaw + Gyro_z * Delta_time) + (1 - GYRO_WEIGHT)*Mag_heading;
+	
+	//printf("Yaw: %lf\n", yaw);
+	
+	IMU_data[0]  = (int)((pitch*(180/PI))*16);
+	IMU_data[1]  = (int)((roll*(180/PI))*16);
+	IMU_data[2]  = (int)((yaw*(180/PI))*16);
+}
+
 //----------------------------------------------------------------------------------//
 /* Set_IMU_mode sets the IMU int Relative orientation Euler angle mode
 *	
--- C_Project/BNO055.h
+++ C_Project/BNO055.h
@@ -3,12 +3,39 @@
 
 //	USER DEFINES	//
 #define EULER_PITCH_H	0x1F
-#define	EULER_PITCH_L	0x1E
-#define EULER_ROLL_H	0x1D
-#define EULER_ROLL_L	0x1C
+#define	EULER_PITCH_L		0x1E
+#define EULER_ROLL_H		0x1D
+#define EULER_ROLL_L		0x1C
 #define EULER_YAW_H		0x1B
 #define EULER_YAW_L		0x1A
 
+#define GYRO_X_L				0x14
+#define GYRO_X_H			0x15
+#define GYRO_Y_L				0x16
+#define GYRO_Y_H			0x17
+#define GYRO_Z_L				0x18
+#define GYRO_Z_H			0x19
+
+#define ACCEL_X_L			0x08
+#define ACCEL_X_H			0x09
+#define ACCEL_Y_L			0x0A
+#define ACCEL_Y_H			0x0B
+#define ACCEL_Z_L			0x0C
+#define ACCEL_Z_H			0x0D
+
+#define MAG_X_L				0x0E
+#define MAG_X_H				0x0F
+#define MAG_Y_L				0x10
+#define MAG_Y_H				0x11
+#define MAG_Z_L				0x12
+#define MAG_Z_H				0x13
+
+#define GYRO_WEIGHT		((float) 0.98)
+
+#ifndef PI
+	#define PI 	((float)3.14159265354252)	
+#endif
+	
 //	SYSTEM DEFINES	//
 
 #define soc_cv_av
@@ -19,6 +46,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <math.h>
 #include "hwlib.h"
 #include "soc_cv_av/socal/socal.h"
 #include "soc_cv_av/socal/hps.h"
@@ -39,5 +67,6 @@
 void BNO055_Map(void* virtual_base);
 void Get_Orientation(int IMU_data[]);
 void Set_NDOF_mode();
+void Get_Complementarty_Orientation(int IMU_data[], float Delta_time);
 //----------------------------------------------------------------------//
 #endif
